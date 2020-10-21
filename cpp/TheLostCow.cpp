cb338b08fa74de55f9b42c26e6a6b857e10a33b4
Kevin Lan <kevinlan321@gmail.com>
2020-10-20 21:14:18 -0700

    solved 'Mixing Milk'

--- cpp/TheLostCow.cpp
+++ cpp/TheLostCow.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
-#include <cstdlib>
-
+#include <cmath>
 using namespace std;
+
 typedef long long ll;
 
 int main() {
-  ll x, y;
-  cin >> x >> y;
+    freopen("lostcow.in", "r", stdin);
+    freopen("lostcow.out", "w", stdout);
 
-  ll ans = 0;
-  ll by = 1;
-  ll dir = 1;
-  while(true) {
-    if((dir==1 && x<=y && y<=x+by) || (dir==-1 && x-by<=y && y<=x)) {
-      ans += abs(y-x);
-      cout << ans << endl;
-      break;
-    } else {
-      ans += by*2;
-      by *= 2;
-      dir *= -1;
+    ll x, y;
+    cin >> x >> y;
+
+    ll ans = 0;
+    ll by = 1;
+    ll dir = 1;
+
+    while(true) {
+        if((dir==1 && x<=y && y<=x+by) || (dir==-1 && x-by<=y && y<=x)) {
+            ans += abs(y-x);
+            cout << ans << endl;
+            break;
+        } 
+        else {
+            ans += by*2;
+            by *= 2;
+            dir *= -1;
+        }
     }
-  }
+    
+    return 0;
 }
