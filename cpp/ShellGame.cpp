7b6babf1c6f67bdba232937443a77d9cf1e2bf29
Kevin Lan <kevinlan321@gmail.com>
2020-10-21 23:01:57 -0700

    solved 'Shell Game'

---
+++ cpp/ShellGame.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include<algorithm>
+using namespace std;
+ 
+int N, A[100], B[100], G[100];
+ 
+int num_correct(int starting_shell)
+{
+  int current_shell = starting_shell, correct = 0;
+  for (int i=0; i<N; i++) {
+    if (A[i] == current_shell) current_shell = B[i];
+    else if (B[i] == current_shell) current_shell = A[i];
+    if (current_shell == G[i]) correct++;
+  }
+  return correct;
+}
+ 
+int main(void)
+{
+  ifstream cin ("shell.in");
+  cin >> N;
+  for (int i=0; i<N; i++)
+    cin >> A[i] >> B[i] >> G[i];
+ 
+  int best = 0;
+  for (int i=1; i<=3; i++) 
+    best = max(best, num_correct(i));
+ 
+  ofstream fout ("shell.out");
+  cout << best << "\n";
+  return 0;
+}
