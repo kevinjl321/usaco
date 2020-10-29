d1bccb70fcdb2df71bfd7ce2dbb7e5e94044c10d
Kevin Lan <kevinlan321@gmail.com>
2020-10-29 08:32:32 -0700

    demo w/promotion counting

--- cpp/PromotionCounting.cpp
+++ cpp/PromotionCounting.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
-  //freopen("promote.in", "r", stdin);
-  //freopen("promote.out", "w", stdout);
+  freopen("promote.in", "r", stdin);
+  freopen("promote.out", "w", stdout);
   
   int b_init, b_fin, s_init, s_fin, g_init, g_fin, p_init, p_fin;
   cin >> b_init >> b_fin >> s_init >> s_fin >> g_init >> g_fin >> p_init >> p_fin;
@@ -13,10 +13,10 @@ int main()
   int g_promote = (p_fin + g_fin)- (p_init + g_init);
   int p_promote = (p_fin - p_init);
   
+  
   cout << s_promote << endl;
   cout << g_promote << endl;
   cout << p_promote << endl;
   
   return 0;
-  
 }
