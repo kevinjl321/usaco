5d8ecae0cee5942abc95aa7b6a9e3866efd7cc0a
Kevin Lan <kevinlan321@gmail.com>
2020-11-04 22:22:14 -0800

    solved 'WDTCCTRII'

---
+++ complete-search/WhyDidTheCowCrossTheRoadII.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    freopen("circlecross.in", "r", stdin);
+    freopen("circlecross.out", "w", stdout);
+
+    string s;
+    cin >> s;
+    
+    int ans = 0;
+    for(int i = 0; i < s.length(); i++){
+        for(int j = i + 1; j < s.length(); j++){
+            for(int k = j + 1; k < s.length(); k++){
+                for(int z = k + 1; z < s.length(); z++){
+                    ans += s[i] == s[k] && s[j] == s[z];
+                }
+            }
+        }
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
