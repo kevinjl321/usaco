5f145871ba4742ada2e0515f28e159a9e143e1ad
Kevin Lan <kevinlan321@gmail.com>
2020-12-19 19:01:18 -0800

    updated template to fit new guidelines

--- template.cpp
+++ template.cpp
@@ -6,16 +6,6 @@
 #include<cmath>
 using namespace std;
 
-void setIO(string name = "") {
-    ios_base::sync_with_stdio(0); cin.tie(0); 
-    if((name).size()){
-        freopen((name+".in").c_str(), "r", stdin); 
-        freopen((name+".out").c_str(), "w", stdout);
-    }
-}
- 
 int main(void){
-    setIO("");
-    
     return 0;
 }
