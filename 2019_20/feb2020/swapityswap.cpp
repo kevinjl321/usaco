7b04b843a3e9f2b7239aaa4b51138be356b56144
Kevin Lan <kevinlan321@gmail.com>
2020-12-28 15:08:49 -0800

    restarted swapity-swap.cpp

--- 2019_20/feb2020/swapityswap.cpp
+++ 2019_20/feb2020/swapityswap.cpp
@@ -1,41 +1,21 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<string>
+#include<cmath>
 using namespace std;
- 
-void setIO(string s) {
-	ios_base::sync_with_stdio(0); cin.tie(0); 
-	freopen((s+".in").c_str(), "r", stdin);
-	freopen((s+".out").c_str(), "w", stdout);
-}
- 
-int N, K, A1, A2, B1, B2, res[101];
- 
-int nex(int x) {
-	if (A1 <= x && x <= A2) {
-        x = A1 + A2 - x;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    if((name).size()){
+        freopen((name+".in").c_str(), "r", stdin); 
+        freopen((name+".out").c_str(), "w", stdout);
     }
-	if (B1 <= x && x <= B2) {
-        x = B1 + B2 - x;
-    } 
-	return x;
 }
- 
-int main() {
-	setIO("swap");
-    
-	cin >> N >> K >> A1 >> A2 >> B1 >> B2;
-	for (int i = 1; i <= N; ++i) {
-		int p = 1, cur = nex(i);
-		while (cur != i) {
-			p++;
-			cur = nex(cur);
-		}
-		int k = K % p;
-		for (int j = 0; j < k; ++j) {
-            cur = nex(cur);
-        } 
-		res[cur] = i;
-	}
-	for (int i = 1; i <= N; ++i) {
-        cout << res[i] << endl;
-    } 
+
+int main(void){
+    setIO("swap");
+	
+    return 0;
 }
