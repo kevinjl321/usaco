b881a8eec0267ba98f98195c57d4e21149fd8602
Kevin Lan <kevinlan321@gmail.com>
2021-01-05 21:05:25 -0800

    solve mixmilk, shellgame

--- 2018_19/dec2018/mixingmilk.cpp
+++ 2018_19/dec2018/mixingmilk.cpp
@@ -1,1 +1,41 @@
-//
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int capacity[3], amount[3];
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    if((name).size()){
+        freopen((name+".in").c_str(), "r", stdin); 
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+void pour(int i, int j){
+    int amt = min(amount[i], capacity[j] - amount[j]);
+    amount[i] -= amt;
+    amount[j] += amt;
+}
+
+int main(void){
+    setIO("mixmilk");
+
+    for(int i = 0; i < 3; i++){
+        cin >> capacity[i] >> amount[i];
+    }
+
+    for(int i = 0; i < 100; i++){
+        pour(i % 3, (i + 1) % 3);
+    }
+
+    for(int i = 0; i < 3; i++){
+        cout << amount[i] << endl;
+    }
+
+    return 0;
+}
--- 2018_19/jan2019/shellgame.cpp
+++ 2018_19/jan2019/shellgame.cpp
@@ -1,1 +1,40 @@
-//
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<string>
+#include<cmath>
+using namespace std;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); 
+    if((name).size()){
+        freopen((name+".in").c_str(), "r", stdin); 
+        freopen((name+".out").c_str(), "w", stdout);
+    }
+}
+
+int main(void){
+    setIO("shell");
+
+    int n;
+    int positions[3];
+    int ctr[3] = {0};
+
+    cin >> n;
+    for(int i = 0; i < 3; i++){
+        positions[i] = i;
+    }
+    
+    for(int i = 0; i < n; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--, b--, c--;
+        
+        swap(positions[a], positions[b]);
+        ctr[positions[c]]++;
+    }
+    
+    cout << max({ctr[0], ctr[1], ctr[2]}) << endl;
+    return 0;
+}
